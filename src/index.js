6b6aec38cb94932009acb86f738ab47d6ae08471
Floris De Feyter <floris_defeyter@hotmail.com>
2021-03-19 12:59:29 +0100

    Replace with CenterNet_ResNet50_v2 detector

--- src/index.js
+++ src/index.js
@@ -5,31 +5,17 @@ import {loadGraphModel} from '@tensorflow/tfjs-converter';
 import "./styles.css";
 tf.setBackend('webgl');
 
-const threshold = 0.75;
+const threshold = 0.5;
 
 async function load_model() {
-    // It's possible to load the model locally or from a repo
-    // You can choose whatever IP and PORT you want in the "http://127.0.0.1:8080/model.json" just set it before in your https server
-    //const model = await loadGraphModel("http://127.0.0.1:8080/model.json");
-    const model = await loadGraphModel("https://raw.githubusercontent.com/hugozanini/TFJS-object-detection/master/models/kangaroo-detector/model.json");
+    const model = await loadGraphModel("models/centernet_resnet50_v2_tfjs/model.json");
+    console.log("Model loaded!");
     return model;
   }
-
-let classesDir = {
-    1: {
-        name: 'Kangaroo',
-        id: 1,
-    },
-    2: {
-        name: 'Other',
-        id: 2,
-    }
-}
 
 class App extends React.Component {
   videoRef = React.createRef();
   canvasRef = React.createRef();
-
 
   componentDidMount() {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -37,10 +23,10 @@ class App extends React.Component {
         .getUserMedia({
           audio: false,
           video: {
-            facingMode: "user"
-          }
+            facingMode: "user",
+          },
         })
-        .then(stream => {
+        .then((stream) => {
           window.stream = stream;
           this.videoRef.current.srcObject = stream;
           return new Promise((resolve, reject) => {
@@ -53,35 +39,48 @@ class App extends React.Component {
       const modelPromise = load_model();
 
       Promise.all([modelPromise, webCamPromise])
-        .then(values => {
+        .then((values) => {
           this.detectFrame(this.videoRef.current, values[0]);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     }
   }
 
-    detectFrame = (video, model) => {
-        tf.engine().startScope();
-        model.executeAsync(this.process_input(video)).then(predictions => {
-        this.renderPredictions(predictions, video);
+  detectFrame = (video, model) => {
+    tf.engine().startScope();
+    model.executeAsync(this.process_input(video))
+    .then((predictions) => {
+      const boxes = predictions[2];
+      const scores = predictions[0];
+      const classes = predictions[3];
+      tf.image.nonMaxSuppressionAsync(
+        boxes.squeeze(),
+        scores.squeeze(),
+        1000
+      ).then((idxs) => {
+        this.renderPredictions(
+          tf.gather(boxes, idxs).arraySync(),
+          tf.gather(scores, idxs).arraySync(),
+          tf.gather(classes, idxs.arraySync()));
         requestAnimationFrame(() => {
           this.detectFrame(video, model);
         });
         tf.engine().endScope();
       });
+    });
   };
 
-  process_input(video_frame){
+  process_input(video_frame) {
     const tfimg = tf.browser.fromPixels(video_frame).toInt();
-    const expandedimg = tfimg.transpose([0,1,2]).expandDims();
-    return expandedimg;
-  };
+    const expandedimg = tfimg.transpose([0, 1, 2]).expandDims();
+    return tf.image.resizeBilinear(expandedimg, [320, 320]).cast('int32');
+  }
 
-  buildDetectedObjects(scores, threshold, boxes, classes, classesDir) {
-    const detectionObjects = []
-    var video_frame = document.getElementById('frame');
+  buildDetectedObjects(scores, threshold, boxes, classes) {
+    const detectionObjects = [];
+    var video_frame = document.getElementById("frame");
 
     scores[0].forEach((score, i) => {
       if (score > threshold) {
@@ -96,16 +95,16 @@ class App extends React.Component {
         bbox[3] = maxY - minY;
         detectionObjects.push({
           class: classes[i],
-          label: classesDir[classes[i]].name,
+          label: "Object",
           score: score.toFixed(4),
-          bbox: bbox
-        })
+          bbox: bbox,
+        });
       }
-    })
-    return detectionObjects
+    });
+    return detectionObjects;
   }
 
-  renderPredictions = predictions => {
+  renderPredictions = (boxes, scores, classes) => {
     const ctx = this.canvasRef.current.getContext("2d");
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
@@ -115,17 +114,18 @@ class App extends React.Component {
     ctx.textBaseline = "top";
 
     //Getting predictions
-    const boxes = predictions[4].arraySync();
-    const scores = predictions[5].arraySync();
-    const classes = predictions[6].dataSync();
-    const detections = this.buildDetectedObjects(scores, threshold,
-                                    boxes, classes, classesDir);
+    const detections = this.buildDetectedObjects(
+      scores,
+      threshold,
+      boxes,
+      classes
+    );
 
-    detections.forEach(item => {
-      const x = item['bbox'][0];
-      const y = item['bbox'][1];
-      const width = item['bbox'][2];
-      const height = item['bbox'][3];
+    detections.forEach((item) => {
+      const x = item["bbox"][0];
+      const y = item["bbox"][1];
+      const width = item["bbox"][2];
+      const height = item["bbox"][3];
 
       // Draw the bounding box.
       ctx.strokeStyle = "#00FFFF";
@@ -134,42 +134,47 @@ class App extends React.Component {
 
       // Draw the label background.
       ctx.fillStyle = "#00FFFF";
-      const textWidth = ctx.measureText(item["label"] + " " + (100 * item["score"]).toFixed(2) + "%").width;
+      const textWidth = ctx.measureText(
+        item["label"] + " " + (100 * item["score"]).toFixed(2) + "%"
+      ).width;
       const textHeight = parseInt(font, 10); // base 10
       ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
     });
 
-    detections.forEach(item => {
-      const x = item['bbox'][0];
-      const y = item['bbox'][1];
+    detections.forEach((item) => {
+      const x = item["bbox"][0];
+      const y = item["bbox"][1];
 
       // Draw the text last to ensure it's on top.
       ctx.fillStyle = "#000000";
-      ctx.fillText(item["label"] + " " + (100*item["score"]).toFixed(2) + "%", x, y);
+      ctx.fillText(
+        item["label"] + " " + (100 * item["score"]).toFixed(2) + "%",
+        x,
+        y
+      );
     });
   };
 
   render() {
     return (
       <div>
-        <h1>Real-Time Object Detection: Kangaroo</h1>
-        <h3>MobileNetV2</h3>
+        <h1>Object Detection</h1>
         <video
-          style={{height: '600px', width: "500px"}}
+          style={{ height: "512px", width: "512px" }}
           className="size"
           autoPlay
           playsInline
           muted
           ref={this.videoRef}
-          width="600"
-          height="500"
+          width="512"
+          height="512"
           id="frame"
         />
         <canvas
           className="size"
           ref={this.canvasRef}
-          width="600"
-          height="500"
+          width="512"
+          height="512"
         />
       </div>
     );
